5a3782776de8b68da3c5070921c2eaf8cf95b09c
Bhavani Varma <116121053+bhavanivarmak@users.noreply.github.com>
2023-01-07 13:31:26 +0530

    Add files via upload

---
+++ 10X-HMS/palindrome.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+function checkPalindrome(string) {
+
+    let len = string.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len - 1 - i]) {
+            return 'NO';
+        }
+    }
+    return 'YES';
+}
+let string = readLine().toLowerCase()
+let ans = checkPalindrome(string);
+console.log(ans);
